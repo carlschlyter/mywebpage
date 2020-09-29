915b164ab50eb6e326eb6a71ed94d05320ac8785
Carl Schlyter <carl.olof.schlyter@hotmail.com>
2020-09-29 11:07:52 +0200

    Continued set up with a few basic templates pages.

--- front-page.php
+++ front-page.php
@@ -1,5 +1,19 @@
 <?php get_header(); ?>
 
+<div class="container">
 
+    <div class="row">
+
+        <div class="col-md-12">
+        
+            <h1> <?php the_title(); ?> </h1>
+
+            <?php get_template_part('includes/section', 'content'); ?>
+
+        </div>        
+        
+    </div>
+
+</div>    
 
 <?php get_footer(); ?>
---
+++ template-contactme.php
@@ -0,0 +1,36 @@
+<?php
+/* 
+Template Name: Contact Me
+*/
+?>
+
+<?php get_header(); ?>
+
+<div class="container">
+
+    <div class="row">
+
+        <div class="col-md-12">
+
+            <h1> <?php the_title(); ?> </h1>    
+        
+        </div>
+
+        <div class="col-md-6">
+        
+            <?php get_template_part('includes/section', 'content'); ?>
+
+        </div>        
+
+        <div class="col-md-6">
+        
+            <p>Det här är den andra sidan</p>            
+
+        </div>        
+        
+    </div>
+
+</div>    
+
+<?php get_footer(); ?>
+

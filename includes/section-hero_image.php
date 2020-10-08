970fb739983aa063f9626f05ca84213a41483eb1
Carl Schlyter <carl.olof.schlyter@hotmail.com>
2020-10-08 16:29:52 +0200

    Added welcome text message and made responsive font adjustments and more..

--- includes/section-hero_image.php
+++ includes/section-hero_image.php
@@ -7,17 +7,30 @@
         $img = get_sub_field('image');
         $pic = $img['sizes']['hero-image-new'];
         $msg = get_sub_field('welcome_message');
+        $txt = get_sub_field('welcome_text');
+        $txt2 = get_sub_field('welcome_text_2');
         
     ?>
 
         <div id="hero-sub-container">
 
-            <div class="col-md-6" id="welcome-message">
+            <div class="col-md-12" id="welcome-message">
+                <div>
+                    <h1> <?php echo $msg; ?> </h1> 
+                </div>
 
-                <h1> <?php echo $msg; ?> </h1> 
+                <div id="welcome-text-message">
+                    <div class="col-md-3 text-box">
+                        <p> <?php echo $txt; ?> </p>    
+                    </div>
+
+                    <div class="col-md-3 text-box">
+                        <p> <?php echo $txt2; ?> </p>    
+                    </div>
+                </div>
 
             </div>
-
+    
             <img src="<?php echo $pic ?>" class="img-fluid">  
         
         </div>

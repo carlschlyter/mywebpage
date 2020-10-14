7af16cb7e9836861a788591b4d4594750c2602ae
Carl Schlyter <carl.olof.schlyter@hotmail.com>
2020-10-14 09:00:45 +0200

    Finalised contact form on front page.

--- includes/section-contact.php
+++ includes/section-contact.php
@@ -1,49 +1,53 @@
 
-<div class="row" id="message-box">
-    <div id="success_message" class="alert alert-success col-md-12" style="display:none"></div>
-</div>
+<div class="container">
 
-<form id="contact">
+    <div class="row" >
+        <div id="success_message" class="alert alert-success col-md-12" style="display:none"></div>
+    </div>
 
-    <h2>Kontakta mig!</h2>
+    <form id="contact">
 
-    <div class="form-group row">
+        <h2>Kontakta mig!</h2>
 
-        <div class="col-md-6">
-            <input type="text" name="Förnamn" placeholder="Förnamn" class="form-control" required>
+        <div class="form-group row">
+
+            <div class="col-md-6">
+                <input type="text" name="Förnamn" placeholder="Förnamn" class="form-control" required>
+            </div>
+            
+            <div class="col-md-6">
+                <input type="text" name="Efternamn" placeholder="Efternamn" class="form-control" required>
+            </div>
+
         </div>
         
-        <div class="col-md-6">
-            <input type="text" name="Efternamn" placeholder="Efternamn" class="form-control" required>
+        <div class="form-group row">
+
+            <div class="col-md-6">
+                <input type="email" name="E-post" placeholder="E-post" class="form-control" required>
+            </div>
+
+            <div class="col-md-6">
+                <input type="tel" name="Mobil" placeholder="Mobil" class="form-control" required>
+            </div>
+
         </div>
 
-    </div>
-     
-    <div class="form-group row">
+        <div class="form-group">
 
-        <div class="col-md-6">
-            <input type="email" name="E-post" placeholder="E-post" class="form-control" required>
+            <textarea name="Meddelande" placeholder="Meddelande" class="form-control" required></textarea>
+
         </div>
 
-        <div class="col-md-6">
-            <input type="tel" name="Mobil" placeholder="Mobil" class="form-control" required>
+        <div class="form-group">
+
+            <button type="submit" class="btn btn-success btn-block">Skicka din fråga</button>
+
         </div>
+    
+    </form>
 
-    </div>
-
-    <div class="form-group">
-
-        <textarea name="Meddelande" placeholder="Meddelande" class="form-control" required></textarea>
-
-    </div>
-
-    <div class="form-group">
-
-        <button type="submit" class="btn btn-success btn-block">Skicka din fråga</button>
-
-    </div>
- 
-</form>
+</div>
 
 <script>
 
@@ -71,13 +75,15 @@
 
             success: function(res){
 
-                $('#contact').fadeOut(300);
+                $('#contact').fadeOut(1000);
 
                 $('#success_message').text('Tack för ditt meddelande').show();
 
                 $('#contact').trigger('reset');
 
-                $('#contact').fadeIn(500);
+                $('#contact').fadeIn(5000);
+
+                $('#success_message').fadeOut(3000);
 
             },
 

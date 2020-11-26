6584e4e0e00a1934f2190463eceb73db114fa738
Carl Schlyter <carl.olof.schlyter@hotmail.com>
2020-11-26 09:11:13 +0100

    Added correct title for website. Changed theme screenshot image.

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Carl Schlyter Webbutveckling</title>
     <link href="https://fonts.googleapis.com/css2?family=Indie+Flower&&family=Montserrat&family=Roboto&display=swap" rel="stylesheet">
 
 <?php wp_head(); ?>
